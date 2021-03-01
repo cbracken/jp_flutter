3d9be7d867043c36c9e09ba9927848a0557290f5
Chris Bracken <cbracken@google.com>
2021-03-01 09:33:28 -0800

    Update UI for better screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Padding(padding: EdgeInsets.symmetric(horizontal: 10), child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -92,23 +92,17 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Row(children: <Widget>[
-              Text("Enter text:"),
-              Expanded(child: TextField(
-                  style: TextStyle(fontFamily: "IPAFont"),
-                ),
+            TextField(
+              style: TextStyle(fontFamily: 'IPAFont', fontSize: 18.0),
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.send),
+                border: const OutlineInputBorder(),
               ),
-            ]),
+            ),
           ],
-        ),
+        )),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
