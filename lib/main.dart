36b093003b4cb4efa573d9ee355ccab50cc1bba6
Chris Bracken <chris@bracken.jp>
2022-06-10 13:57:43 -0700

    HACK

--- lib/main.dart
+++ lib/main.dart
@@ -6,42 +6,12 @@ void main() {
   runApp(MyApp());
 }
 
-//class IOSCompositionWidget extends StatelessWidget {
-//  const IOSCompositionWidget({super.key});
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    // This is used in the platform side to register the view.
-//    const String viewType = 'FlutterNativeLabel';
-//
-//    // Pass parameters to the platform side.
-//    final Map<String, dynamic> creationParams = <String, dynamic>{};
-//
-//    return UiKitView(
-//      viewType: viewType,
-//      layoutDirection: TextDirection.ltr,
-//      creationParams: creationParams,
-//      creationParamsCodec: const StandardMessageCodec(),
-//    );
-//  }
-//}
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -52,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -69,77 +30,63 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _textureId = 0;
+  String _status = 'Nothing to see here';
+  static const foo = MethodChannel('foo');
+
+  Future<void> loadImage(String imageUrl) async {
+    return foo.invokeMethod('registerTexture', <String, dynamic> {
+      'url': imageUrl,
+    });
+  }
+
+  Future<int> getTextureId() async {
+    return foo.invokeMethod('getTextureId').then((textureId) {
+      return textureId as int;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      if (_counter == 1) {
+        //String imageUrl = 'https://chris.bracken.jp/post/2005-04-09-sakura.jpg';
+        String imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Tree_example_VIS.jpg/640px-Tree_example_VIS.jpg';
+        loadImage(imageUrl).then((_) {
+          print('Loaded $imageUrl');
+          setState(() {
+            _status = 'loaded $imageUrl';
+          });
+        });
+      } else {
+        getTextureId().then((textureId) {
+          print('Got textureId in dart: $textureId');
+          setState(() {
+            _textureId = textureId;
+          });
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Padding(padding: EdgeInsets.symmetric(horizontal: 10), child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TextField(
-              style: TextStyle(fontFamily: 'IPAFont', fontSize: 18.0),  // Japanese
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.send),
-                border: const OutlineInputBorder(),
-              ),
-            ),
-            TextField(
-              style: TextStyle(fontFamily: 'Nanum', fontSize: 18.0),    // Korean
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.send),
-                border: const OutlineInputBorder(),
-              ),
-            ),
-            //SizedBox(
-            //  height: 48,
-            //  child: IOSCompositionWidget(),
-            //),
+            Text(_status),
             Container(
               decoration: BoxDecoration(border: Border.all(color: Colors.black)),
               child: SizedBox(
-                width: 100,
-                height: 50,
-                child: Texture(textureId: 10),
+                width: 640,
+                height: 480,
+                child: Texture(textureId: _textureId),
               ),
             ),
           ],
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,11 +4,56 @@ import FlutterMacOS
 class JunkTexture: NSObject, FlutterTexture {
   var pixelBuffer: CVPixelBuffer?;
 
-  override init() {
+  init(url: String) {
     super.init()
 
-    CVPixelBufferCreate(kCFAllocatorDefault,
-                        100, 50, kCVPixelFormatType_32BGRA, nil, &pixelBuffer)
+    do {
+      //let url = URL(string: "https://chris.bracken.jp/post/2005-04-09-sakura.jpg");
+      let image = NSImage(data: try Data(contentsOf: URL(string: url)!))!
+      print("Got the image! ", String(image.description))
+
+      let attrs = [
+        kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+        kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue,
+        kCVPixelBufferMetalCompatibilityKey: kCFBooleanTrue] as CFDictionary
+
+      let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                          Int(image.size.width),
+                          Int(image.size.height),
+                          kCVPixelFormatType_32BGRA,
+                          attrs,
+                          &pixelBuffer)
+      guard let resultPixelBuffer = pixelBuffer, status == kCVReturnSuccess else {
+        NSLog("Bad news. CVPixelBuffer creation failed!")
+        return
+      }
+
+      CVPixelBufferLockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+      let pixelData = CVPixelBufferGetBaseAddress(resultPixelBuffer)
+      let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+      guard let context = CGContext(data: pixelData,
+                                    width: Int(image.size.width),
+                                    height: Int(image.size.height),
+                                    bitsPerComponent: 8,
+                                    bytesPerRow: CVPixelBufferGetBytesPerRow(resultPixelBuffer),
+                                    space: rgbColorSpace,
+                                    bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+        NSLog("Pretty bad news. We made it as far as creating a graphics context, then died.")
+        return
+      }
+
+      let graphicsContext = NSGraphicsContext(cgContext: context, flipped: false)
+      NSGraphicsContext.saveGraphicsState()
+      NSGraphicsContext.current = graphicsContext
+      image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+      NSGraphicsContext.restoreGraphicsState()
+      CVPixelBufferUnlockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+    } catch {
+      print(error)
+    }
+
+//    CVPixelBufferCreate(kCFAllocatorDefault,
+//                        100, 50, kCVPixelFormatType_32BGRA, nil, &pixelBuffer)
   }
 
   func copyPixelBuffer() -> Unmanaged<CVPixelBuffer>? {
@@ -17,7 +62,9 @@ class JunkTexture: NSObject, FlutterTexture {
 }
 
 class MainFlutterWindow: NSWindow {
-  let junkTexture = JunkTexture()
+  var junkTexture: JunkTexture?
+  var channel: FlutterMethodChannel?
+  var textureId: Int64?
 
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
@@ -26,12 +73,34 @@ class MainFlutterWindow: NSWindow {
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
-    weak var registrar = flutterViewController.registrar(forPlugin: "FooPlugin")
 
-    let textureId = registrar?.textures.register(junkTexture)
-    if let id = textureId {
-      print("Texture ID is: ", String(id))
-    }
+    // HACK HACK HACK
+    weak var registrar = flutterViewController.registrar(forPlugin: "FooPlugin")
+    channel = FlutterMethodChannel(name: "foo", binaryMessenger: registrar!.messenger)
+    channel!.setMethodCallHandler({ [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      switch call.method {
+      case "registerTexture":
+        let args = call.arguments as? Dictionary<String, Any>
+        let imageUrl = args!["url"] as? String
+        self?.junkTexture = JunkTexture(url: imageUrl!)
+        if let weakself = self {
+          weak var registrar = (weakself.contentViewController as? FlutterViewController)!.registrar(forPlugin: "FooPlugin")
+          weakself.textureId = registrar!.textures.register(weakself.junkTexture!)
+          NSLog("Texture ID is: %@", weakself.textureId!)
+        } else {
+          NSLog("Couldn't get weakself")
+        }
+        result("Loaded image!")
+      case "getTextureId":
+        if let weakself = self {
+          NSLog("Called getTextureId! Returning %@", weakself.textureId!)
+        }
+        result(self?.textureId!)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    })
+    // HACK HACK HACK
 
     super.awakeFromNib()
   }
